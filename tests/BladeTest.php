ca5cc1fcfad14742055d4bbeb3985517ef583c13
Tikam <tikamshah@gmail.com>
2023-08-20 21:19:21 +0530

    Update BladeTest.php

--- tests/BladeTest.php
+++ tests/BladeTest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Caffeinated\Shinobi\Tests;
+namespace Tikamsah\Shinobi\Tests;
 
-use Caffeinated\Shinobi\Tests\User;
-use Caffeinated\Shinobi\Models\Role;
-use Caffeinated\Shinobi\Tests\TestCase;
-use Caffeinated\Shinobi\Models\Permission;
+use Tikamsah\Shinobi\Tests\User;
+use Tikamsah\Shinobi\Models\Role;
+use Tikamsah\Shinobi\Tests\TestCase;
+use Tikamsah\Shinobi\Models\Permission;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BladeTest extends TestCase
